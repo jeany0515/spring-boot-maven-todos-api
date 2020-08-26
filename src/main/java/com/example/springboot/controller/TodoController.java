d04193dd5a4c7d2814d2c41255012e3ef2823027
devxu <devxu@thoughtworks.com>
2020-08-26 11:45:46 +0800

    [todos api][jeany]: add todos api

---
+++ src/main/java/com/example/springboot/controller/TodoController.java
@@ -0,0 +1,57 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.model.Todo;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    public List<Todo> todoList;
+
+    public TodoController() {
+        this.todoList = getTodos();
+    }
+
+    public List<Todo> getTodos() {
+        List<Todo> todoList = new ArrayList<>();
+        Todo todo0 = new Todo();
+        todo0.setId(0);
+        todo0.setDone(false);
+        todo0.setText("this is my first todo");
+        todoList.add(todo0);
+
+        Todo todo1 = new Todo();
+        todo1.setId(1);
+        todo1.setDone(false);
+        todo1.setText("this is my second todo");
+        todoList.add(todo1);
+
+        return todoList;
+    }
+
+    @GetMapping
+    public List<Todo> getAll() {
+        return todoList;
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Todo create(@RequestBody Todo todo) {
+        todoList.add(todo);
+
+        return todo;
+    }
+
+    @PutMapping("/{todoId}")
+    public Todo update(@PathVariable Integer todoId, @RequestBody Todo newTodo) {
+        Todo oldTodo = todoList.stream().filter(todo -> todoId.equals(todo.getId())).findFirst().get();
+        int index = todoList.indexOf(oldTodo);
+        todoList.set(index, newTodo);
+
+        return newTodo;
+    }
+}
---
+++ src/main/java/com/example/springboot/model/Todo.java
@@ -0,0 +1,31 @@
+package com.example.springboot.model;
+
+public class Todo {
+    private Integer id;
+    private String text;
+    private Boolean done;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+}
